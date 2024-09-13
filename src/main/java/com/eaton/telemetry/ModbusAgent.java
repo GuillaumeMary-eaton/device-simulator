0d4929b1435987c4a8134d8359613907a1bb0a70
Guillaume Mary <guillaumemary@eaton.com>
2024-09-13 18:23:23 +0200

    Reviewing Modbus agent implementation. Handling coils request.

--- src/main/java/com/eaton/telemetry/ModbusAgent.java
+++ src/main/java/com/eaton/telemetry/ModbusAgent.java
@@ -1,24 +1,26 @@
 package com.eaton.telemetry;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
 
-import net.solarnetwork.io.modbus.ModbusBlockType;
+import net.solarnetwork.io.modbus.BitsModbusMessage;
 import net.solarnetwork.io.modbus.ModbusErrorCode;
+import net.solarnetwork.io.modbus.ModbusMessage;
 import net.solarnetwork.io.modbus.RegistersModbusMessage;
 import net.solarnetwork.io.modbus.netty.msg.BaseModbusMessage;
 import net.solarnetwork.io.modbus.tcp.netty.NettyTcpModbusServer;
 
+import static net.solarnetwork.io.modbus.netty.msg.BitsModbusMessage.*;
 import static net.solarnetwork.io.modbus.netty.msg.RegistersModbusMessage.readHoldingsResponse;
-import static net.solarnetwork.io.modbus.netty.msg.RegistersModbusMessage.readInputsRequest;
 
 public class ModbusAgent {
 
-    record ModbusSensor(int sensorId, RegisterType registerType, Supplier<short[]> dataSupplier) {
+    record ModbusSensor(int sensorId, RegisterType registerType, Supplier<Object> dataSupplier) {
 
-        public short[] getValue() {
+        public Object getValue() {
             return dataSupplier.get();
         }
     }
@@ -35,37 +37,31 @@ public class ModbusAgent {
         this.server = new NettyTcpModbusServer(port);
     }
 
-    public void addSensor(int sensorId, RegisterType registerType, Supplier<short[]> dataSupplier) {
+    public void addSensor(int sensorId, RegisterType registerType, Supplier<Object> dataSupplier) {
         sensors.put(sensorId, new ModbusSensor(sensorId, registerType, dataSupplier));
     }
 
     public void configure() {
         server.setMessageHandler((msg, sender) -> {
-            // this handler only supports read holding registers requests
-            RegistersModbusMessage req = msg.unwrap(RegistersModbusMessage.class);
-            if (req != null && req.getFunction().blockType() == ModbusBlockType.Holding) {
-                ModbusSensor modbusSensor = sensors.get(req.getUnitId());
-                // respond with the fake data
-                net.solarnetwork.io.modbus.netty.msg.RegistersModbusMessage modbusMessage = null;
-                switch (req.getFunction().blockType()) {
-                    case Holding, Input -> modbusMessage = readHoldingsResponse(req.getUnitId(), req.getAddress(), modbusSensor.getValue());
-                    case Coil, Discrete -> {
-                        modbusMessage = readInputsRequest(req.getUnitId(), req.getAddress(), 1);
-                    }
-                    case Diagnostic -> {
-                        sender.accept(new BaseModbusMessage(msg.getUnitId(), msg.getFunction(), ModbusErrorCode.Acknowledge));
-                    }
+            ModbusMessage modbusMessage = null;
+            ModbusSensor modbusSensor = sensors.get(msg.getUnitId());
+            switch (msg.getFunction().blockType()) {
+                case Coil, Discrete -> {
+                    BitsModbusMessage tcpRequest = msg.unwrap(BitsModbusMessage.class);
+                    modbusMessage = readCoilsResponse(tcpRequest.getUnitId(), tcpRequest.getAddress(), tcpRequest.getCount(), (BigInteger) modbusSensor.getValue());
                 }
-//                switch (modbusSensor.registerType()) {
-//                    case H -> modbusMessage = readHoldingsResponse(req.getUnitId(), req.getAddress(), modbusSensor.getValue());
-//                    case I, C, D -> modbusMessage = readInputsRequest(req.getUnitId(), req.getAddress(), 1);
-//                }
-                sender.accept(modbusMessage);
-            } else {
-                // send back error that we don't handle that request
-                sender.accept(new BaseModbusMessage(msg.getUnitId(), msg.getFunction(),
-                        ModbusErrorCode.IllegalFunction));
+                case Holding -> {
+                    RegistersModbusMessage registerRequest = msg.unwrap(RegistersModbusMessage.class);
+                    modbusMessage = readHoldingsResponse(registerRequest.getUnitId(), registerRequest.getAddress(), (short[]) modbusSensor.getValue());
+                }
+                case Input -> {
+                    throw new UnsupportedOperationException("Not yet implemented");
+                }
+                case Diagnostic -> {
+                    modbusMessage = new BaseModbusMessage(msg.getUnitId(), msg.getFunction(), ModbusErrorCode.Acknowledge);
+                }
             }
+            sender.accept(modbusMessage);
         });
     }
 
